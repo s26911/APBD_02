87368b4337b01768361944fd7e364036e7dc8b48
Jakub Gołębiewski <kgolebiewski00@gmail.com>
2024-03-14 22:57:21 +0100

    Dodano srednia

--- APBD_application/APBD_application/Program.cs
+++ APBD_application/APBD_application/Program.cs
@@ -1,6 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Modyfikacja 1");
-Console.WriteLine("Modyfikacja 2");
-Console.WriteLine("Modyfikacja 3");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] tab = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+        Console.WriteLine();
+        Console.WriteLine("Średnia: " + Srednia(tab));
+    }
+
+    static double Srednia(int[] tab)
+    {
+        double sum = 0;
+        foreach (var i in tab)
+            sum += i;
+
+        return sum / tab.Length;
+    }
+}
